df0098d95e231f6c192df72f3638594260580652
JuRibeiro12 <juliana.ribeiropaiva@gmail.com>
2022-06-22 08:17:39 -0300

    Projeto iniciado

---
+++ inicial.js
@@ -0,0 +1,51 @@
+/* var largura;
+var comprimento
+var area;
+
+largura=20;
+comprimento=10;
+area = largura * comprimento;
+
+console.log("A área é de "+area+"mts²") */
+
+/* var ferradura;
+var quantidade;
+
+ferradura = 4
+quantidade = 4 * 10
+
+console.log("Você precisará de "+quantidade+" ferraduras") */
+
+/* var broa;
+var pao;
+var poupança;
+var arrecadação;
+
+broa = 1.5 * 25
+pao = 0.12 * 200
+arrecadação = pao + broa
+poupança = arrecadação * 0.1
+
+console.log("Você arrecadou "+arrecadação+" reais no total e precisa guardar na poupançça "+poupança+" reais.") */
+
+/* var nome;
+var idade;
+var dv;
+
+nome = 'Juliana' 
+idade = 20
+dv = 365 * idade
+
+console.log(""+nome+", você já viveu "+dv+" dias.") */
+
+/* var gasolina;
+var litros;
+var pagamento;
+
+gasolina = 7.4
+litros = 13
+pagamento = gasolina * litros
+
+console.log("Você terá que pagar "+pagamento+" reais.") */
+
+
