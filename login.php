c3ed530cebf7a0fc248abc5ffb361eda7f08b959
JULIANA RIBEIRO PAIVA <37646938866@pesqueira.ifpe>
2022-10-13 12:09:44 -0300

    iniciou-se a programação da pagina da volts

--- login.php
+++ login.php
@@ -9,17 +9,21 @@
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
 </head>
 <body>
+    <div class="container"> 
+      <div class="row">
+            <div class="col-6">
 <table class="table table-striped">
   <thead>
     <tr> 
         <th>#</th>
         <th>Usuário</th>
-        <th>Registro</th>
+        <th>Registro Entrada</th>
+        <th>Registro Saida</th>
     </tr>
   </thead>
   <tbody>
 <?php
-$mysqli = new mysqli('localhost:3307', 'root', '', 'teste1');
+$mysqli = new mysqli('localhost:3306', 'root', '', 'pesquisa_dev');
 $user = $_POST['usuario'];
 $pass = $_POST['senha'];
 $result = $mysqli->query("SELECT * FROM usuarios WHERE usuario = '$user' AND senha = '$pass';");
@@ -31,14 +35,27 @@ if($result->num_rows == 1){
     $isercao =  "INSERT INTO ponto (id_usuario) VALUES (" . $usuario['id'] . ");";
     $mysqli->query($isercao);
     $result2 = $mysqli->query("SELECT * FROM ponto WHERE id_usuario = " . $usuario['id'] . ";");
+    $contador = 1;
+    $linha = 1;
     while ($ponto = $result2->fetch_array()){
+        if($contador%2!=0){
+            $contador++;
         ?>
     <tr> 
-        <td><?php echo $ponto['id']?></td>
+        <td><?php echo $linha?></td>
         <td><?php echo $ponto['id_usuario']?></td>
         <td><?php echo $ponto['registro']?></td>
-    </tr>
+
         <?php
+        }else{
+            $contador++;
+            $linha++;
+        ?>
+         <td><?php echo $ponto['registro']?></td>
+        </tr>
+    
+        <?php
+     }
     }
 }else{
     echo 'Usuário Negado';
@@ -57,6 +74,9 @@ while ($usuario = $result->fetch_array()){
 
   </tbody>
 </table>
+</div>
+</div>
+</div>
 </body>
 <!-- JavaScript Bundle with Popper -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
